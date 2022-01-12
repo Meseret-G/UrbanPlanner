78b2d837e307310a92972853ffea21dcec71c21f
Messi_G <negash4@gmail.com>
2022-01-11 23:16:09 -0600

    Add project files.

---
+++ UrbanPlanner/Models/Building.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner.Models
+{
+    class Building
+    {
+        //overloading 
+        public Building()
+        {
+
+        }
+        //Constructor
+        public Building (string address)
+        {
+            _address = address;
+            
+        }
+        public Building(string designer, DateTime dateConstructed, string address, string owner)
+        {
+            _designer = designer;
+            _dateConstructed = dateConstructed;
+            _address = address;
+            _owner = owner;
+
+        }
+        //fields
+        private string _designer = "Meseret";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+    
+        // properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public  double Volume
+        {
+            get =>  Width * Depth * (3 * Stories);
+                  
+            }
+       
+        //Methods
+        public void Construct ()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase(string name)
+        {
+            _owner = name;
+        }
+        public void PrintToConsole()
+        {
+            string output = $@"
+{_address } --------
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner }
+{Volume} cubic meters of space";
+
+Console .WriteLine(output);
+        }
+        }
+    }
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UrbanPlanner.Models;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate - use the 'new' keyword to create an instance of an object 
+            // initialize - give an instance an intial value 
+            Building oneTwoThree = new Building("123 main street");
+
+            oneTwoThree.Width = 5;
+            oneTwoThree.Stories = 2;
+            oneTwoThree.Depth = 10;
+
+            Building fourFiveSix = new Building("456 main street")
+            {
+                Width = 7,
+                Depth = 4,
+                Stories = 3
+        };
+            Building differentOne = new Building("1023 main street")
+            {
+                Width = 6,
+                Depth = 15,
+                Stories = 1
+            };
+            Building shertLocksPlace = new Building("221B Baker street")
+            {
+                Width = 7,
+                Depth = 14,
+                Stories = 2
+            };
+            oneTwoThree.Construct();
+            fourFiveSix.Construct();
+            shertLocksPlace.Construct();
+
+            oneTwoThree.Purchase(" Scrooge");
+            //@ - string verbatim 
+            oneTwoThree.PrintToConsole();
+
+            fourFiveSix.Purchase(" Myself");
+            fourFiveSix.PrintToConsole();
+
+            shertLocksPlace.Purchase(" Myself");
+            shertLocksPlace.PrintToConsole();
+
+
+
+        }
+    }
+}
